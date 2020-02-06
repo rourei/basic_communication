c04c1d02ddeb0629977c37e2d48780226498333b
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-02-06 15:18:45 +0100

    renamed repo

--- src/client.cpp
+++ src/client.cpp
@@ -4,14 +4,14 @@
 
 #include <ros/ros.h>
 #include <dynamic_reconfigure/client.h>
-#include <dynamic_tutorials/TutorialsConfig.h>
+#include <basic_communication/TutorialsConfig.h>
 
-dynamic_tutorials::TutorialsConfig CFG;
+basic_communication::TutorialsConfig CFG;
 dynamic_reconfigure::ConfigDescription DESCRPT;
 
 
 // ########### Callbacks ###########
-void configurationCallback(const dynamic_tutorials::TutorialsConfig& config){
+void configurationCallback(const basic_communication::TutorialsConfig& config){
     ROS_INFO("configurationCallback: %d %f %s %s %d",
              config.int_param, config.double_param,
              config.str_param.c_str(),
@@ -30,14 +30,14 @@ void descriptionCallback(const dynamic_reconfigure::ConfigDescription& descripti
 int main(int argc, char **argv) {
 
     // Initialize ROS, NodeHandle necessary for using
-    ros::init(argc, argv, "dynamic_tutorials_client");
+    ros::init(argc, argv, "basic_communication_client");
     ros::NodeHandle n;
 
     // Define loop rate
     ros::Rate loop_rate(0.1);
 
     // Create client and config object
-    dynamic_reconfigure::Client<dynamic_tutorials::TutorialsConfig> client("/dynamic_tutorials_server", &configurationCallback, &descriptionCallback);
+    dynamic_reconfigure::Client<basic_communication::TutorialsConfig> client("/basic_communication_server", &configurationCallback, &descriptionCallback);
 
 
     // DEBUG
@@ -72,6 +72,10 @@ int main(int argc, char **argv) {
             // Change paramter in config
             CFG.int_param = CFG.int_param + 3;
         }
+		else if (!client.getCurrentConfiguration(CFG, ros::Duration(10)))
+		{
+			ROS_INFO("Timeout in loop.");
+		}
 
         // DEBUG
         ROS_INFO("New configuration (inside loop): %d %f %s %s %d",
--- src/server.cpp
+++ src/server.cpp
@@ -5,11 +5,11 @@
 
 #include <ros/ros.h>
 #include <dynamic_reconfigure/server.h>
-#include <dynamic_tutorials/TutorialsConfig.h>
+#include <basic_communication/TutorialsConfig.h>
 
 
 // Callback will be called when new configuration is received
-void callback(dynamic_tutorials::TutorialsConfig &config, uint32_t level) {
+void callback(basic_communication::TutorialsConfig &config, uint32_t level) {
     // For now, it's only printig the new configuration
     ROS_INFO("Reconfigure Request: %d %f %s %s %d",
              config.int_param, config.double_param,
@@ -19,12 +19,12 @@ void callback(dynamic_tutorials::TutorialsConfig &config, uint32_t level) {
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "dynamic_tutorials_server");
+    ros::init(argc, argv, "basic_communication_server");
 
     // Create server object, passing the config type
-    dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig> server;
+    dynamic_reconfigure::Server<basic_communication::TutorialsConfig> server;
     // Creating the callback that is called upon receiving a new config
-    dynamic_reconfigure::Server<dynamic_tutorials::TutorialsConfig>::CallbackType f;
+    dynamic_reconfigure::Server<basic_communication::TutorialsConfig>::CallbackType f;
 
     // Variable to represent callback
     f = boost::bind(&callback, _1, _2);
